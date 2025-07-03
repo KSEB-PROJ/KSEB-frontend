eaed71df09c3961ea823ee6acc2d4b5db61212a8
JaeHyunAn <98042706+yyuneu@users.noreply.github.com>
2025-07-04 07:19:54 +0900

    메인페이지 임시
    ㅇㅇㅇㅇ

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import HomePage from './pages/HomePage';
-import './App.css';
+import React, { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import MainPage from './pages/MainPage';
 
-function App() {
-  return (
-    <HomePage />
-  );
-}
+const App: React.FC = () => {
+  useEffect(() => {
+    // 1. Lenis 인스턴스 생성 및 설정
+    const lenis = new Lenis();
+
+    // 2. GSAP ScrollTrigger가 Lenis를 사용하도록 설정 (매우 중요!)
+    lenis.on('scroll', ScrollTrigger.update);
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+    gsap.ticker.lagSmoothing(0);
+
+
+    // 3. 애니메이션 프레임마다 Lenis 업데이트
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    // 4. 컴포넌트 언마운트 시 Lenis 인스턴스 정리
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return <MainPage />;
+};
 
 export default App;
