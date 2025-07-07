97216e747e82fccd821fead308f111a393a50d86
JaeHyunAn <98042706+yyuneu@users.noreply.github.com>
2025-07-08 01:26:53 +0900

    대시보드 영역 기본 틀 사이드마 애니메이션 추가

---
+++ src/pages/SchedulePage/SchedulePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SchedulePage: React.FC = () => {
+    return (
+        <div style={{ padding: '2rem' }}>
+            <h1>개인 스케줄 입력</h1>
+            <p>이곳에서 개인 스케줄을 추가하고 관리할 수 있습니다.</p>
+            {/* 캘린더 라이브러리 등이 이곳에 추가될 예정. */}
+        </div>
+    );
+};
+
+export default SchedulePage;
